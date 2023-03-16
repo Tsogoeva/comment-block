8751c5a99c02ff2b858cc0ad9739f376bb84667a
Tatiana Tsogoeva <tsogoevats@gmail.com>
2023-03-16 20:46:45 +0700

    add likeIcon, import current date, fix adding likes, getting number of comments, fix view

--- src/view.js
+++ src/view.js
@@ -1,7 +1,10 @@
 import onChange from 'on-change';
 
 import heartSvg from './images/heart.svg';
+import likedHeartSvg from './images/liked-heart.svg';
 import trashSvg from './images/trash.svg';
+
+import { getCurrentDate, getCurrentFormatedDate } from './init.js';
 
 function createComment(comment) {
 	const container = document.createElement('div');
@@ -19,49 +22,15 @@ function createComment(comment) {
 	const footer = document.createElement('div');
 	footer.classList.add('comment__item_footer');
 
-	// const likeBlock = document.createElement('div');
-	// likeBlock.classList.add('comment__item_like');
-
 	const likeImg = document.createElement('img');
 	likeImg.classList.add('comment__item_like');
 	likeImg.setAttribute('src', heartSvg);
 	likeImg.setAttribute('alt', 'Нравится');
-	// likeBlock.append(likeImg);
 
 	const removeImg = document.createElement('img');
 	removeImg.classList.add('comment__item_remove');
 	removeImg.setAttribute('src', trashSvg);
 	removeImg.setAttribute('alt', 'Удалить');
-
-	// const removeSvg = document.createElement('svg');
-	// removeSvg.classList.add('comment__item_remove');
-	// removeSvg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-	// removeSvg.setAttribute('id', 'Outline');
-	// removeSvg.setAttribute('viewBox', '0 0 24 24');
-	// removeSvg.setAttribute('width', '15');
-
-	// const trashPath1 = document.createElement('path');
-	// trashPath1.setAttribute('d', 'M21,4H17.9A5.009,5.009,0,0,0,13,0H11A5.009,5.009,0,0,0,6.1,4H3A1,1,0,0,0,3,6H4V19a5.006,5.006,0,0,0,5,5h6a5.006,5.006,0,0,0,5-5V6h1a1,1,0,0,0,0-2ZM11,2h2a3.006,3.006,0,0,1,2.829,2H8.171A3.006,3.006,0,0,1,11,2Zm7,17a3,3,0,0,1-3,3H9a3,3,0,0,1-3-3V6H18Z');
-	// const trashPath2 = document.createElement('path');
-	// trashPath2.setAttribute('d', 'M10,18a1,1,0,0,0,1-1V11a1,1,0,0,0-2,0v6A1,1,0,0,0,10,18Z');
-	// const trashPath3 = document.createElement('path');
-	// trashPath3.setAttribute('d', 'M14,18a1,1,0,0,0,1-1V11a1,1,0,0,0-2,0v6A1,1,0,0,0,14,18Z');
-
-	// removeSvg.append(trashPath1, trashPath2, trashPath3);
-
-	// console.log(removeSvg)
-
-	// <svg xmlns = "http://www.w3.org/2000/svg" 
-	// id = "Outline" 
-	// viewBox = "0 0 24 24" 
-	// width = "15" 
-	// fill = "#b7b6b6" >
-	// <path d="M21,4H17.9A5.009,5.009,0,0,0,13,0H11A5.009,5.009,0,0,0,6.1,4H3A1,1,0,0,0,3,6H4V19a5.006,5.006,0,0,0,5,5h6a5.006,5.006,0,0,0,5-5V6h1a1,1,0,0,0,0-2ZM11,2h2a3.006,3.006,0,0,1,2.829,2H8.171A3.006,3.006,0,0,1,11,2Zm7,17a3,3,0,0,1-3,3H9a3,3,0,0,1-3-3V6H18Z" />
-	// <path d="M10,18a1,1,0,0,0,1-1V11a1,1,0,0,0-2,0v6A1,1,0,0,0,10,18Z" />
-	// <path d="M14,18a1,1,0,0,0,1-1V11a1,1,0,0,0-2,0v6A1,1,0,0,0,14,18Z" /></ >
-
-
-
 
 	const date = document.createElement('span');
 	date.classList.add('comment__item_date');
@@ -90,92 +59,120 @@ function likeComments(likedIds) {
 		const likeImg = tag.querySelector('.comment__item_like');
 
 		if (likedIds.includes(tagId)) {
-			likeImg.classList.add('liked');
+			const likedImg = document.createElement('img');
+			likedImg.classList.add('comment__item_like');
+			likedImg.setAttribute('src', likedHeartSvg);
+			likedImg.setAttribute('alt', 'Нравится');
+			likeImg.replaceWith(likedImg);
+
 		} else {
-			likeImg.classList.remove('liked');
+			const unlikedImg = document.createElement('img');
+			unlikedImg.classList.add('comment__item_like');
+			unlikedImg.setAttribute('src', heartSvg);
+			unlikedImg.setAttribute('alt', 'Нравится');
+
+			likeImg.replaceWith(unlikedImg);
 		}
 	})
 }
 
-// function addNotice(field) {
-// }
+const getStringInCorrectCase = (int) => {
+	const words = ['комментарий', 'комментария', 'комментариев'];
 
-// function removeNotice(field) {
+	if (int === 0) {
+		return 'Нет ' + words[2];
+	}
 
-// }
+	const number = int + ' ';
+	const remainderOf100 = int % 100;
+	const remainderOf10 = remainderOf100 % 10;
+
+	if (remainderOf100 > 10 && remainderOf100 < 20) {
+		return number + words[2];
+	}
+
+	if (remainderOf10 > 1 && remainderOf10 < 5) {
+		return number + words[1];
+	}
+
+	if (remainderOf10 === 1) {
+		return number + words[0];
+	}
+
+	return number + words[2];
+}
 
 export default (elements, state) => onChange(state, (path, value) => {
+	const {
+		lineWithNumberOfComments,
+		container,
+		name,
+		noticeName,
+		date,
+		textArea,
+		noticeTextArea,
+		form,
+	} = elements;
 
-	console.log('VIEWWWWWWW')
-
-	console.log(path)
-	console.log(value)
-
-	const { button, textArea, container, form, name, date } = elements;
+	const {
+		comments,
+		likeCommentIds,
+		numberOfComments,
+	} = state;
 
 	switch (path) {
 		case 'comments':
-			renderComments(state.comments, container);
+			renderComments(comments, container);
+			likeComments(likeCommentIds);
+			break;
+
+		case 'numberOfComments':
+			lineWithNumberOfComments.textContent = getStringInCorrectCase(numberOfComments);
 			break;
 
 		case 'likeCommentIds':
-			likeComments(state.likeCommentIds);
+			likeComments(likeCommentIds);
 			break;
 
-		case 'focusedField.name':
-			state.focusedField.name.classList.remove('invalid');
+		case 'validationField.name.focused':
+			if (value) {
+				name.classList.remove('invalid');
+				noticeName.style.display = 'none';
+			}
+			break;
 
-			if (state.invalidField.textArea) {
-				button.setAttribute('disabled', '');
+		case 'validationField.name.valid':
+			if (value) {
+				name.classList.remove('invalid');
+				noticeName.style.display = 'none';
 			} else {
-				button.removeAttribute('disabled');
+				name.classList.add('invalid');
+				noticeName.style.display = 'block';
 			}
 			break;
 
-		case 'focusedField.textArea':
-			state.focusedField.textArea.classList.remove('invalid');
+		case 'validationField.textArea.focused':
+			if (value) {
+				textArea.classList.remove('invalid');
+				noticeTextArea.style.display = 'none';
+			}
+			break;
 
-			if (state.invalidField.name) {
-				button.setAttribute('disabled', '');
+		case 'validationField.textArea.valid':
+			if (value) {
+				textArea.classList.remove('invalid');
+				noticeTextArea.style.display = 'none';
 			} else {
-				button.removeAttribute('disabled');
+				textArea.classList.add('invalid');
+				noticeTextArea.style.display = 'block';
 			}
 			break;
 
-		case 'invalidField.name':
-			state.invalidField.name.classList.add('invalid');
-			button.setAttribute('disabled', '');
+		case 'process':
+			form.reset();
+			name.focus();
+			date.value = getCurrentFormatedDate(getCurrentDate());
 			break;
-
-		case 'invalidField.textArea':
-			state.invalidField.textArea.classList.add('invalid');
-			button.setAttribute('disabled', '');
-			break;
-
-		case 'process': {
-			switch (value) {
-				case 'receiving':
-					button.setAttribute('disabled', '');
-					name.setAttribute('readonly', '');
-					date.setAttribute('readonly', '');
-					textArea.setAttribute('readonly', '');
-					break;
-
-				case 'received':
-					button.removeAttribute('disabled');
-					name.removeAttribute('readonly');
-					date.removeAttribute('readonly');
-					textArea.removeAttribute('readonly');
-					form.reset();
-					textArea.focus();
-					break;
-
-				default:
-					throw new Error('Unknown process!');
-
-			}
-			break;
-		}
 
 		default:
 			throw new Error('Unknown state!');
